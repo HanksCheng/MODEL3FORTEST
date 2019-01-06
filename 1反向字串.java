24f02dff0f9ab0eef24fd381ec0c671a9bc75836
HanksCheng <43198381+HanksCheng@users.noreply.github.com>
2019-01-07 00:39:20 +0800

    Add files via upload

--- 1反向字串.java
+++ 1反向字串.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        fun(str);
+    }
+    public static void fun(String str) {
+        char data[] = str.toCharArray();
+        for (int i=data.length-1;i>=0;i--){
+            System.out.print(data[i]);
+        }
+        System.out.println();
     }
 }
